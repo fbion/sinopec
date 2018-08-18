13de9bed85e8150d2dab8dff0a7c6c4cf1df3862
xzc <593793109@qq.com>
2018-08-18 10:31:51 +0800

    添加自定义banner

--- src/main/java/com/sensenets/sinopec/config/FastjsonConfig.java
+++ src/main/java/com/sensenets/sinopec/config/FastjsonConfig.java
@@ -4,7 +4,6 @@ import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.serializer.ValueFilter;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
-import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter4;
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
